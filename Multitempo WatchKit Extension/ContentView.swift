14c0aba8b070293f726b787afb599a2cdfe76d6b
Dmitriy Loktev <dmitry.loktev@jetbrains.com>
2021-04-17 10:29:48 +0300

    Initial Commit

---
+++ Multitempo WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Multitempo WatchKit Extension
+//
+//  Created by Dmitriy Loktev on 17.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Multitempo WatchKit Extension/MultitempoApp.swift
@@ -0,0 +1,19 @@
+//
+//  MultitempoApp.swift
+//  Multitempo WatchKit Extension
+//
+//  Created by Dmitriy Loktev on 17.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct MultitempoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
